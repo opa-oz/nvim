3082c8c71aa7ad8090af69a4939a6f9f3747e3be
Vladimir Levin <vladimirlevinozinki@gmail.com>
2024-06-01 19:11:41 +0900

    Harpoon yo

--- lua/opa-oz/plugins/init.lua
+++ lua/opa-oz/plugins/init.lua
@@ -23,6 +23,7 @@ local plugins = {
     require("opa-oz.plugins.nvim-oil"),
     require("opa-oz.plugins.nvim-toggleterm"),
     require("opa-oz.plugins.undotree"),
+    require("opa-oz.plugins.harpoon"),
 }
 
 return plugins
