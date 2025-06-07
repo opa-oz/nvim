8f44bd16b12dded1df9bcff67b23b1e1b16e8f0c
Vladimir Levin <opaozhub@gmail.com>
2025-06-07 17:34:15 +0900

    feat: Custom `uci` files

--- lua/opa-oz/filetype/uci.lua
+++ lua/opa-oz/filetype/uci.lua
@@ -6,14 +6,30 @@ if filetype then
     filetype.add({
         pattern = {
             [".*/openwrt/dhcp"] = "uci",
+            [".*/openwrt/dropbear"] = "uci",
             [".*/openwrt/firewall"] = "uci",
+            [".*/openwrt/network"] = "uci",
+            [".*/openwrt/rpcd"] = "uci",
+            [".*/openwrt/system"] = "uci",
+            [".*/openwrt/ucitrack"] = "uci",
+            [".*/openwrt/uhttpd"] = "uci",
+            [".*/openwrt/luci"] = "uci",
+            [".*/*.uci"] = "uci",
         },
     })
 else
     api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = {
             "*/openwrt/dhcp",
+            "*/openwrt/dropbear",
             "*/openwrt/firewall",
+            "*/openwrt/network",
+            "*/openwrt/rpcd",
+            "*/openwrt/system",
+            "*/openwrt/ucitrack",
+            "*/openwrt/uhttpd",
+            "*/openwrt/luci",
+            "*.uci",
         },
         callback = function()
             bo.filetype = "uci"
